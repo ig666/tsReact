c156471cbc9778f8cc8cc8aa64e951418459e317
Qi Chen <qi.chen>
2020-12-23 00:45:47 +0800

    modify 增加自动完成组件

--- dragon-ships/src/components/AutoComplete/autoComplete.stories.tsx
+++ dragon-ships/src/components/AutoComplete/autoComplete.stories.tsx
@@ -1,35 +1,27 @@
-import React from 'react';
+import React from "react";
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta } from '@storybook/react/types-6-0';
+import { Story, Meta } from "@storybook/react/types-6-0";
 import { action } from '@storybook/addon-actions'
-
-import {AutoComplete, AutoCompleteProps } from './autoComplete';
+import { AutoComplete, AutoCompleteProps } from "./autoComplete";
 
 export default {
-  title: 'Example/AutoComplete',
+  title: "Example/AutoComplete",
   component: AutoComplete,
 } as Meta;
 
 const Template: Story<AutoCompleteProps> = (args) => <AutoComplete {...args} />;
-const lakers = [
-    "bradley",
-    "pope",
-    "caruso",
-    "cook",
-    "cousins",
-    "james",
-    "AD",
-    "green",
-    "howard",
-    "kuzma",
-    "McGee",
-    "rando",
-  ];
-  const filterChange = (val: string) => {
-    return lakers.filter((item) => item.includes(val));
-  };
+const handleFetch = (query: string) => {
+  return fetch(`https://api.github.com/search/users?q=${query}`)
+    .then((res) => res.json())
+    .then(({ items }) => {
+      console.log(items);
+      return items
+        .slice(0, 10)
+        .map((item: any) => ({ value: item.login, ...item }));
+    });
+};
 export const Primary = Template.bind({});
 Primary.args = {
-    filterChange,
-    onSelect:action('selected')
+  fetchSuggestions: handleFetch,
+  onSelect:action('selected')
 };
